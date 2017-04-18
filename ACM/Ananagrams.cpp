89de039ad01cc71e529f69ef358f39b87e11cc9c
alll4u <837204976@qq.com>
2017-04-18 12:07:38 +0800

    Ugly Numbers

--- ACM/Ananagrams.cpp
+++ ACM/Ananagrams.cpp
@@ -15,28 +15,31 @@ string repr(string& str){
 
 vector<string> words;
 map<string, int> cnt;
-int main(){
-
-	string s;
-
-	//讲输入循环读入到vector中，将格式化的字符串加入到map中，顺便统计字符个数
-	while (cin >> s){
-		if (s[0] == '#') break;
-		words.push_back(s);
-		string r = repr(s);
-		//添加一个新的map项目
-		if (!cnt.count(r)) cnt[r] = 0;
-		cnt[r]++;
-	}
-
-	vector<string> ans;
-	for (int i = 0; i < words.size();i++)
-		if (cnt[repr(words[i])] == 1) 
-			ans.push_back(words[i]);
-	sort(ans.begin(), ans.end());
-	for (int i = 0; i < ans.size(); i++)
-		cout << ans[i] << endl;
-
-	system("pause");
-	return 0;
-}
+//int main(){
+//
+//	string s;
+//
+//	//讲输入循环读入到vector中，将格式化的字符串加入到map中，顺便统计字符个数
+//	while (cin >> s){
+//		if (s[0] == '#') break;
+//		words.push_back(s);
+//		string r = repr(s);
+//		//添加一个新的map项目
+//		if (!cnt.count(r)) cnt[r] = 0;
+//		cnt[r]++;
+//	}
+//
+//	vector<string> ans;
+//	//如果字典中记录的个数为1，则加入到ans向量
+//	for (int i = 0; i < words.size();i++)
+//		if (cnt[repr(words[i])] == 1) 
+//			ans.push_back(words[i]);
+//	sort(ans.begin(), ans.end());
+//
+//	//循环输出
+//	for (int i = 0; i < ans.size(); i++)
+//		cout << ans[i] << endl;
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ ACM/UglyNumbers.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<queue>
+#include <functional>
+#include <set>
+using namespace std;
+typedef long long ntype;
+const int coeff[3] = { 2, 3, 5 };
+
+
+int main(){
+
+
+	priority_queue<ntype, vector<ntype>, greater<ntype> > pq;
+	set<ntype> ans;
+
+	pq.push(1);
+	ans.insert(1);
+	for (int i = 1;; i++){
+		ntype var = pq.top();
+		pq.pop();
+		if (i == 1500) {
+			cout << "ok!" << "num is" << var << endl;
+			break;
+		}
+		
+		for (int i = 0; i < 3; i++){
+			ntype num = var * coeff[i];
+			if (!ans.count(num)){
+				ans.insert(num);
+				pq.push(num);
+				cout << "num is " << num << endl;
+			}
+		}
+	}
+
+
+	system("pause");
+	return 0;
+}
